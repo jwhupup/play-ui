6904aa178f900e7131c3f31f9a4479bd342a16f8
xz_jinweihua <1945623533@qq.com>
2023-02-23 18:00:23 +0800

    chore: update commitlint.

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,4 +1,6 @@
-module.exports = {
+import type { UserConfig } from '@commitlint/types'
+
+const Configuration: UserConfig = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [
@@ -19,8 +21,10 @@ module.exports = {
     'type-empty': [0],
     'scope-empty': [0],
     'scope-case': [0],
-    'subject-full-stop': [0, 'never'],
-    'subject-case': [0, 'never'],
+    'subject-full-stop': [0, 'never', ''],
+    'subject-case': [0, 'never', []],
     'header-max-length': [0, 'always', 72],
   },
 }
+
+module.exports = Configuration
