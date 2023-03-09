127d34e27a1194cdaa6872493f0108f175e44ff8
jwhupup <1945623533@qq.com>
2023-03-09 23:15:03 +0800

    chore: rebuild docs

--- docs/example/button-group/sizes.vue
+++ docs/example/button-group/sizes.vue
@@ -11,10 +11,48 @@
         <pl-button>
           button
         </pl-button>
+        <pl-button>
+          button
+        </pl-button>
       </pl-button-group>
     </div>
     <div fscw gap-2>
       <pl-button-group type="outline">
+        <pl-button>
+          button
+        </pl-button>
+        <pl-button>
+          button
+        </pl-button>
+        <pl-button>
+          button
+        </pl-button>
+        <pl-button>
+          button
+        </pl-button>
+      </pl-button-group>
+    </div>
+    <div fscw gap-2>
+      <pl-button-group type="solid" vertical>
+        <pl-button>
+          button
+        </pl-button>
+        <pl-button>
+          button
+        </pl-button>
+        <pl-button>
+          button
+        </pl-button>
+        <pl-button>
+          button
+        </pl-button>
+      </pl-button-group>
+    </div>
+    <div fscw gap-2>
+      <pl-button-group type="outline" vertical>
+        <pl-button>
+          button
+        </pl-button>
         <pl-button>
           button
         </pl-button>
