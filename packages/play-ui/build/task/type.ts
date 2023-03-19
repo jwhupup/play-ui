28f51030020de6f39893a6b08d9075e107cd1f8e
jwhupup <1945623533@qq.com>
2023-03-19 17:57:37 +0800

    refactor: refactor some components

--- packages/play-ui/build/task/type.ts
+++ packages/play-ui/build/task/type.ts
@@ -1,13 +1,11 @@
 import path from 'node:path'
-import { copy, remove } from 'fs-extra'
+import { copy } from 'fs-extra'
 import consola from 'consola'
 import { buildOutput } from '../path'
 
 export async function buildType() {
   consola.info('Start copy types...')
   const src = path.resolve(buildOutput, 'types')
-  await copy(`${src}/components`, src, { recursive: true })
-  await remove(`${src}/components`)
   Promise.all(
     ['es', 'lib'].map(dir =>
       copy(src, path.resolve(buildOutput, dir), { recursive: true }),
