7b6cbd92fce4633b3c7647542f0dff8e0891877f
jwhupup <1945623533@qq.com>
2023-06-14 23:51:20 +0800

    chore: write components

--- packages/play-ui/components/alert/src/index.vue
+++ packages/play-ui/components/alert/src/index.vue
@@ -1,43 +1,46 @@
 <template>
-  <div
-    ref="alertEl"
-    class="pl-alert"
-    :class="[`pl-alert--${type}`, `pl-alert--${state}`]"
-  >
-    <Icon v-if="icon" :name="icon" />
-    <div>
-      <slot name="title" />
-      <span v-if="!$slots.title && title" class="pl-alert--title">
-        {{ title }}
-      </span>
-      <slot />
-      <div v-if="!$slots.default && description" class="pl-alert--description">
-        {{ description }}
+  <Transition :leave-active-class="animation('fadeOut')">
+    <div
+      v-if="visible"
+      ref="alertEl"
+      class="pl-alert"
+      :class="[`pl-alert--${type}`, `pl-alert--${state}`]"
+    >
+      <Icon v-if="icon" :name="icon" />
+      <div>
+        <slot name="title" />
+        <span v-if="!$slots.title && title" class="pl-alert--title">
+          {{ title }}
+        </span>
+        <slot />
+        <div v-if="!$slots.default && description" class="pl-alert--description">
+          {{ description }}
+        </div>
       </div>
+      <Button v-if="closable" type="link" state="info" icon-left="x-lg" @click="handleClose" />
     </div>
-    <Button v-if="closable" type="link" state="info" icon-left="x-lg" @click="handleClose" />
-  </div>
+  </Transition>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
 import Button from '../../button/src/index.vue'
 import Icon from '../../icon/src/index.vue'
-import type { AlertProps } from '../../component'
+import { type AlertProps } from '../../component'
+import { animation } from '../../../utils'
 
 withDefaults(defineProps<AlertProps>(), {
   type: 'solid',
   state: 'info',
 })
-
 const emits = defineEmits(['close'])
-
 defineOptions({ name: 'Alert' })
 
+const visible = ref(true)
 const alertEl = ref<HTMLDivElement>()
 
 const handleClose = (evt: MouseEvent) => {
-  alertEl.value?.parentElement?.removeChild(alertEl.value)
+  visible.value = false
   emits('close', evt)
 }
 </script>
