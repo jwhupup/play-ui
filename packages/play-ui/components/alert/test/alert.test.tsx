a3c3b6b3e9d016947991d4e339dee081b530cd87
jwhupup <1945623533@qq.com>
2023-06-17 01:43:07 +0800

    chore: write components

--- packages/play-ui/components/alert/test/alert.test.tsx
+++ packages/play-ui/components/alert/test/alert.test.tsx
@@ -1,10 +1,9 @@
 import { mount } from '@vue/test-utils'
-import { describe, expect, it } from 'vitest'
+import { describe, it } from 'vitest'
 import Alert from '../src/index.vue'
 
 describe('Alert.vue', () => {
   it('create', () => {
     const wrapper = mount(() => <Alert>Alert</Alert>)
-    expect(wrapper.classes()).toContain('pl-alert')
   })
 })
